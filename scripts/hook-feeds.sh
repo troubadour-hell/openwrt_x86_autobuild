fcbe4e4e70d06e6542a1455abb30b0da22fa83bc
Jerome <toplane@pku.edu.cn>
2022-09-15 12:58:25 +0800

    fix hook-feeds.sh

--- scripts/hook-feeds.sh
+++ scripts/hook-feeds.sh
@@ -10,7 +10,7 @@ git clone --depth=1 https://github.com/immortalwrt/openwrt-tmate
 # Add gotop
 rm -rf packages/admin/gotop
 svn co https://github.com/immortalwrt/packages/branches/openwrt-18.06/admin/gotop packages/admin/gotop
-
+popd
 
 # Set to local feeds
 pushd customfeeds/packages
